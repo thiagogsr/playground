dec5020878ed4c605526ff594da56928b8084bb6
Thiago Guimarães <thiagogsr@gmail.com>
2017-08-29 11:20:08 -0300

    Set my state asynchronous

--- lib/playground.ex
+++ lib/playground.ex
@@ -10,19 +10,27 @@ defmodule Playground do
   end
 
   def get_my_state(process_id) do
-    GenServer.call(process_id, {:get_the_state})
+    GenServer.call(process_id, :get_the_state)
   end
 
   def set_my_state(process_id, new_state) do
     GenServer.call(process_id, {:set_the_state, new_state})
   end
 
-  def handle_call({:get_the_state}, _from, my_state) do
+  def set_my_state(:async, process_id, new_state) do
+    GenServer.cast(process_id, {:set_the_state, new_state})
+  end
+
+  def handle_call(:get_the_state, _from, my_state) do
     {:reply, my_state, my_state}
   end
 
   def handle_call({:set_the_state, new_state}, _from, my_state) do
     response = "State changed from #{my_state} to #{new_state}"
     {:reply, response, new_state}
+  end
+
+  def handle_cast({:set_the_state, new_state}, _state)  do
+    {:noreply, new_state}
   end
 end
